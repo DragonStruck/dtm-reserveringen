2aa385fb4be60be756cb84952a91bedd9db47951
Luuk Bouma <luuk.bouma@student.hu.nl>
2023-09-19 09:56:16 +0200

    transferred all contest of index.js to main.js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,12 @@
-document.getElementById("testbutton").addEventListener('click', () => {
-    alert("test");
-})
+import {Device} from "./device.js";
+
+// bind the greeter to the input name and output element
+const device = new Device(
+    document.getElementById('tileDisplay')
+);
+
+//using i = 1 bc the database start at index 1
+for (let i = 1; i <= 38; i++) {
+    await device.fetch();
+    device.render()
+}
