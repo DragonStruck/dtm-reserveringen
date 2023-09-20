19ea63f27ba175b9251d472ec0dedf6c83ea82c2
awakentoast <luuk.bouma01@gmail.com>
2023-09-20 17:16:55 +0200

    now loops dynamically over products in db

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,21 +2,23 @@ import {Device} from "./device.js";
 
 // bind the greeter to the input name and output element
 const device = new Device(
-    document.getElementById('tileDisplay')
+    document.getElementById("products")
 );
-
+let amount;
 try {
+    console.log("Product amount")
     const response = await fetch("/product/count");
     console.log("Product amount: response is ok? " + response.ok + "Status code " + response.status);
 
     const json = await response.json();
     console.log("Product amount: got a json response; " + JSON.stringify(json));
+    amount = json;
 }
 catch (ex) {
     console.log("Something went wrong retrieving in fetch() amount . Exception message is '" + ex.message + "'");
 }
 
-for (let i = 0; i < 1; i++) {
+for (let i = 0; i < amount; i++) {
     await device.fetch();
     device.render()
 }
