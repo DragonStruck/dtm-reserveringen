8aa5bcd77ae34fb77093045b7bbe6100cd41b3b7
DragonStruck <gijsbertmorsing@gmail.com>
2023-10-02 14:36:51 +0200

    changed javascript for retrieving products

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,71 @@
+import {Device} from "./device.js";
+
+let productsContainer = document.getElementById("products");
+
+// Get amount of products
+
+let amount;
+try {
+    console.log("Product amount")
+    const response = await fetch("/product/amount");
+    console.log("Product amount: response is ok? " + response.ok + " Status code " + response.status);
+
+    const json = await response.json();
+    console.log("Product amount: got a json response; " + JSON.stringify(json));
+    amount = json;
+}
+catch (ex) {
+    console.log("Something went wrong retrieving in fetch() amount . Exception message is '" + ex.message + "'");
+}
+
+// Get products
+
+// sessionStorage.removeItem("products");
+
+let products = [];
+
+if (sessionStorage.getItem("products") != null) {
+    console.log("products sessionStorage is not empty");
+
+    products = JSON.parse(sessionStorage.getItem("products"));
+
+} else {
+    console.log("products sessionStorage is empty, retrieving products from database");
+
+    try {
+        for (let i = 0; i < amount; i++) {
+            let device = new Device();
+            products.push(await device.fetch(i + 1));
+        }
+        sessionStorage.setItem("products", JSON.stringify(products));
+    } catch (ex) {
+        console.log("Something went wrong retrieving in fetch() amount . Exception message is '" + ex.message + "'");
+    }
+}
+
+// Show Products
+
+let generateProducts =()=>{
+    return (productsContainer.innerHTML = products.map((product)=>{
+        return `
+        <a href="/product?id=${product.id}" class="product">
+            <img class="product-image" src="${product.imagePaths[0]}" alt="${product.imageAltTexts[0]}">
+            <div class="product-text">
+                <h1>${product.name}</h1>
+                <p>${product.description}</p>
+            </div>
+            <div class="status">
+                <!--<div class="status-background"></div>
+                <span class="status-text">${product.status}</span>-->
+            </div>
+        </a>
+        `;
+    }).join(""));
+}
+
+if (products.length > 0) {
+    generateProducts();
+} else {
+    console.log("No products found");
+}
+
--- src/main/resources/static/js/info.js
+++ src/main/resources/static/js/info.js
@@ -1,5 +1,7 @@
 import {Device} from "./device.js";
 
+let productContainer = document.getElementById("product-container");
+
 function findGetParameter(parameterName) {
     var result = null,
         tmp = [];
@@ -19,5 +21,169 @@ const device = new Device(
 
 const product = findGetParameter('id')
 
-await device.fetch(product);
-device.renderInfo();
+let productData = await device.fetch(product);
+// device.renderInfo();
+
+
+// Show Products
+
+let generateProduct =()=>{
+    return productContainer.innerHTML = `
+        <div id="product" class="product">
+            <div class="product-info-left">
+                <div class="title-container">
+                    <h1 id="product-title">${productData.name}</h1>
+                    <div class="status">
+                        <div class="status-background"></div>
+                        <span id="status-text" class="status-text">${productData.status}</span>
+                    </div>
+                </div>
+                <div class="slideshow-container">
+                    <div id="slides-container" class="slides-container"></div>
+                    <a class="prev" onclick="plusSlides(-1)">❮</a>
+                    <a class="next" onclick="plusSlides(1)">❯</a>
+                    <div id="thumbnail-container" class="row"></div>
+                </div>
+                <div class="product-info-description">
+                    <h2>Product omschrijving</h2>
+                    <p id="description-text">${productData.description}</p>
+                </div>
+            </div>
+            <div class="product-info-right">
+                <div class="product-details">
+                    <h2>Product details</h2>
+                    <p id="details-text">${productData.details}</p>
+                </div>
+                <div class="product-contents">
+                    <h2>Product inhoud</h2>
+                    <p id="contents-text">${productData.contents}</p>
+                </div>
+                <div class="add-to-cart">
+                    <button class="add-to-cart-button"><img src="./icons/cart-outline-white.svg" alt="Calender Icon"> Toevoegen aan mandje</button>
+                </div>
+            </div>
+        </div>
+        `;
+}
+
+if (productData != null) {
+
+    // Generate product
+    generateProduct();
+
+    // Add product images
+    const slides = document.getElementById('slides-container');
+    const thumbnails = document.getElementById('thumbnail-container');
+
+    for (let i = 0; i < productData.imagePaths.length; i++) {
+        const slide = document.createElement("div");
+        slide.classList.add("slides");
+        if (i == 0) {slide.style = "display: block;";}
+
+        const slide_image = document.createElement("img");
+        slide_image.src = productData.imagePaths[i];
+        slide_image.alt = productData.imageAltTexts[i];
+        slide.appendChild(slide_image);
+        slides.appendChild(slide);
+
+        const thumbnail = document.createElement('div');
+        thumbnail.classList.add('column');
+        thumbnail.classList.add('cursor');
+        thumbnail.addEventListener('click', () => {currentSlide(i+1)});
+
+        const thumbnail_image = document.createElement('img');
+        thumbnail_image.classList.add('thumbnail');
+        thumbnail_image.src = productData.imagePaths[i];
+        thumbnail_image.alt = productData.imageAltTexts[i];
+        thumbnail.appendChild(thumbnail_image);
+        thumbnails.appendChild(thumbnail);
+    }
+
+    // Add status backgound
+    const product = document.getElementById('product');
+
+    switch(productData.status) {
+        case "Op voorraad":
+            product.classList.add("available");
+            break;
+        case "In verwerking":
+            product.classList.add("processing");
+            break;
+        case "Goedgekeurd":
+            product.classList.add("processing");
+        break;
+        case "Uitgeleend":
+            product.classList.add("unavailable");
+            break;
+        case "Kwijt":
+            product.classList.add("unavailable");
+            break;
+    }
+
+} else {
+    console.log("No product found");
+}
+
+
+// if (productData != null) {
+
+
+
+
+
+    
+//     document.getElementById('product-title').textContent = productData.name;
+
+//     const product = document.getElementById('product');
+
+//     switch(productData.status) {
+//         case "Op voorraad":
+//             product.classList.add("available");
+//             break;
+//         case "In verwerking":
+//             product.classList.add("processing");
+//             break;
+//         case "Goedgekeurd":
+//             product.classList.add("processing");
+//            break;
+//         case "Uitgeleend":
+//             product.classList.add("unavailable");
+//             break;
+//         case "Kwijt":
+//             product.classList.add("unavailable");
+//             break;
+//     }
+
+//     document.getElementById('status-text').textContent = productData.status;
+
+//     const slides = document.getElementById('slides-container');
+//     const thumbnails = document.getElementById('thumbnail-container');
+
+//     for (let i = 0; i < productData.imagePaths.length; i++) {
+//         const slide = document.createElement("div");
+//         slide.classList.add("slides");
+//         if (i == 0) {slide.style = "display: block;";}
+
+//         const slide_image = document.createElement("img");
+//         slide_image.src = productData.imagePaths[i];
+//         slide_image.alt = productData.imageAltTexts[i];
+//         slide.appendChild(slide_image);
+//         slides.appendChild(slide);
+
+//         const thumbnail = document.createElement('div');
+//         thumbnail.classList.add('column');
+//         thumbnail.classList.add('cursor');
+//         thumbnail.addEventListener('click', () => {currentSlide(i+1)});
+
+//         const thumbnail_image = document.createElement('img');
+//         thumbnail_image.classList.add('thumbnail');
+//         thumbnail_image.src = productData.imagePaths[i];
+//         thumbnail_image.alt = productData.imageAltTexts[i];
+//         thumbnail.appendChild(thumbnail_image);
+//         thumbnails.appendChild(thumbnail);
+//     }
+
+//     document.getElementById('description-text').textContent = productData.description;
+//     document.getElementById('details-text').textContent = productData.details;
+//     document.getElementById('contents-text').textContent = productData.contents;
+// }
