12ff8829bf763eeb940bd617b5f563936c97a976
awakentoast <luuk.bouma01@gmail.com>
2023-10-03 16:10:59 +0200

    deleted changes made locally

--- src/main/java/nl/hu/adsd/dtmreserveringen/DtmReservationsConfig.java
+++ src/main/java/nl/hu/adsd/dtmreserveringen/DtmReservationsConfig.java
@@ -1,6 +1,5 @@
 package nl.hu.adsd.dtmreserveringen;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
