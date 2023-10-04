4e55c90e61a822c575a9c8b09abb199f43525e2b
awakentoast <luuk.bouma01@gmail.com>
2023-10-04 21:33:16 +0200

    improved the way product object was made, and made some small code changed

--- src/main/resources/static/js/info-page.js
+++ src/main/resources/static/js/info-page.js
@@ -3,7 +3,7 @@ import {Product} from "../classes/product.js";
 let productContainer = document.getElementById("product-container");
 
 function findGetParameter(parameterName) {
-    var result = null,
+    let result = null,
         tmp = [];
     location.search
         .substr(1)
@@ -17,17 +17,11 @@ function findGetParameter(parameterName) {
     return result;
 }
 
-const product = new Product(
-    document.getElementById('product')
+try {
+    const product = new Product();
+    const productId = findGetParameter('id');
+    await product.fetch(productId);
 
-);
-
-const productId = findGetParameter('id')
-
-let productData = await product.fetch(productId);
-
-if (productData != null) {
-    // Generate product
     productContainer.innerHTML = product.generateProductInfoPage();
     document.getElementById('loader').style.display = "none";
 
@@ -35,51 +29,41 @@ if (productData != null) {
     const slides = document.getElementById('slides-container');
     const thumbnails = document.getElementById('thumbnail-container');
 
-    for (let i = 0; i < productData.imagePaths.length; i++) {
+    for (let i = 0; i < product.imagePaths.length; i++) {
         const slide = document.createElement("div");
         slide.classList.add("slides");
-        if (i == 0) {slide.style = "display: block;";}
+        if (i === 0) {
+            slide.style.display = "block";
+        }
 
         const slide_image = document.createElement("img");
-        slide_image.src = productData.imagePaths[i];
-        slide_image.alt = productData.imageAltTexts[i];
+        slide_image.src = product.imagePaths[i];
+        slide_image.alt = product.imageAltTexts[i];
         slide.appendChild(slide_image);
         slides.appendChild(slide);
 
         const thumbnail = document.createElement('div');
         thumbnail.classList.add('column');
         thumbnail.classList.add('cursor');
-        thumbnail.addEventListener('click', () => {currentSlide(i+1)});
+        thumbnail.addEventListener('click', () => {
+            currentSlide(i + 1)
+        });
 
         const thumbnail_image = document.createElement('img');
         thumbnail_image.classList.add('thumbnail');
-        thumbnail_image.src = productData.imagePaths[i];
-        thumbnail_image.alt = productData.imageAltTexts[i];
+        thumbnail_image.src = product.imagePaths[i];
+        thumbnail_image.alt = product.imageAltTexts[i];
         thumbnail.appendChild(thumbnail_image);
         thumbnails.appendChild(thumbnail);
     }
+} catch (error) {
+    console.log("something went wrong when tring to display product info: " + error);
+}
 
-    // Add status backgound
-    const product = document.getElementById('product');
 
-    switch(productData.status) {
-        case "Op voorraad":
-            product.classList.add("available");
-            break;
-        case "In verwerking":
-            product.classList.add("processing");
-            break;
-        case "Goedgekeurd":
-            product.classList.add("processing");
-        break;
-        case "Uitgeleend":
-            product.classList.add("unavailable");
-            break;
-        case "Kwijt":
-            product.classList.add("unavailable");
-            break;
-    }
-
-} else {
+if (product == null) {
     console.log("No product found");
+} else {
+    // Generate product
+
 }
