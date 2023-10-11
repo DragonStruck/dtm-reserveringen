39e42313dad587d7e6e3ab5c20f855459a720a8e
awakentoast <luuk.bouma01@gmail.com>
2023-10-11 17:11:57 +0200

    changed storage keys to an enum for consistency

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,18 +1,20 @@
+import {StorageKeys} from "../ENUM/storageKeys";
+
 let cartAmountElement = document.getElementById('cart-amount');
 let cartLocalStorage;
 
-if (localStorage.getItem('cart') == null) {
-    localStorage.setItem('cart', '{"items":[]}');
+if (localStorage.getItem(StorageKeys.CART) == null) {
+    localStorage.setItem(StorageKeys.CART, '{"items":[]}');
 }
 
-cartLocalStorage = JSON.parse(localStorage.getItem('cart'));
+cartLocalStorage = JSON.parse(localStorage.getItem(StorageKeys.CART));
 
 updateCartNumber();
 
 function addToCart(i) {
     if (!inArray(i, cartLocalStorage.items)) {
         cartLocalStorage.items.push(i);
-        localStorage.setItem('cart', JSON.stringify(cartLocalStorage));
+        localStorage.setItem("cart", JSON.stringify(cartLocalStorage));
         console.log(cartLocalStorage.items);
         updateCartNumber();
         return true;
@@ -27,7 +29,7 @@ function removeFromCart(i) {
     const cartItem = cartLocalStorage.items.indexOf(i);
     if (cartItem > -1) {
         cartLocalStorage.items.splice(cartItem, 1);
-        localStorage.setItem('cart', JSON.stringify(cartLocalStorage));
+        localStorage.setItem("cart", JSON.stringify(cartLocalStorage));
         console.log(cartLocalStorage.items);
         // updateCartNumber();
         location.reload();
@@ -40,7 +42,7 @@ function removeFromCart(i) {
 }
 
 function emptyCart() {
-    localStorage.setItem('cart', '{"items":[]}');
+    localStorage.setItem(StorageKeys.CART, '{"items":[]}');
 }
 
 function updateCartNumber() {
@@ -58,7 +60,7 @@ function inArray(item, array) {
 }
 
 
-console.log(JSON.parse(localStorage.getItem('cart')));
+console.log(JSON.parse(localStorage.getItem(StorageKeys.CART)));
 // emptyCart();
 // addToCart(1);
 // addToCart(6);
