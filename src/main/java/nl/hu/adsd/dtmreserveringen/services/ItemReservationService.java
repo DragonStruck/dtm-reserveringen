7f5f0fb2c37cfb9ac809f4d84e5baf7e8756c4e0
DragonStruck <gijsbertmorsing@gmail.com>
2023-10-24 09:53:09 +0200

    small header change

--- src/main/java/nl/hu/adsd/dtmreserveringen/services/ItemReservationService.java
+++ src/main/java/nl/hu/adsd/dtmreserveringen/services/ItemReservationService.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
