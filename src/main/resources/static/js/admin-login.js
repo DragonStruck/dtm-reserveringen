6b8adff291bd98003f6508cd06b143c1fa29b8e8
awakentoast <luuk.bouma01@gmail.com>
2023-10-30 14:43:38 +0100

    admin can now login if correct passwrod, password is stored in .env, retrieved using springboot as the jas files dont have a scoe outside of resources

--- src/main/resources/static/js/admin-login.js
+++ src/main/resources/static/js/admin-login.js
@@ -1,9 +1,9 @@
 const adminPasswordField = document.getElementById("admin-password");
 const adminPasswordButton = document.getElementById("admin-password-button");
 
-adminPasswordButton.addEventListener("click", e => {
+adminPasswordButton.addEventListener("click", async e => {
     e.preventDefault();
-    if (passwordCheck(adminPasswordField.textContent)) {
+    if (await passwordCheck(adminPasswordField.value)) {
         window.location.href = "/admin";
     }
 });
@@ -15,8 +15,22 @@ adminPasswordField.addEventListener("keypress", e => {
     }
 });
 
-function passwordCheck(password) {
-    return true;
+async function passwordCheck(password) {
+    const response = await fetch('admin/login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(password),
+    });
+
+    if (!response.ok) {
+        console.log("All reservations: response is error; Status code: " + response.status);
+        return false;
+    } else {
+        return await response.json();
+    }
 }
 
 
